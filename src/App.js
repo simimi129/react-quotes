98c3bfd02ab86909b6b58efb99b307724a2893c1
simimi129 <simimi129@gmail.com>
2023-01-04 09:58:02 +0100

    developing

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import { Route } from "react-router-dom";
-import Welcome from "./components/Welcome";
-import Products from "./components/Products";
+import Welcome from "./pages/Welcome";
+import Products from "./pages/Products";
+import MainHeader from "./components/MainHeader";
 
 function App() {
   return (
     <div>
+      <MainHeader />
       <Route path="/welcome">
         <Welcome />
       </Route>
---
+++ src/components/MainHeader.js
@@ -0,0 +1,19 @@
+import React from "react";
+import { Link } from "react-router-dom";
+
+function MainHeader() {
+  return (
+    <header>
+      <nav>
+        <ul>
+          <li>
+            <Link to="/welcome">Welcome</Link>
+            <Link to="/products">Products</Link>
+          </li>
+        </ul>
+      </nav>
+    </header>
+  );
+}
+
+export default MainHeader;
