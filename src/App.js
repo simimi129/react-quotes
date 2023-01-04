15fe38fcc130ec9df134046b9aef6ac985325db7
simimi129 <simimi129@gmail.com>
2023-01-04 10:13:56 +0100

    developing

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import { Route } from "react-router-dom";
+import { Route, Switch } from "react-router-dom";
 import Welcome from "./pages/Welcome";
 import Products from "./pages/Products";
 import MainHeader from "./components/MainHeader";
@@ -8,15 +8,17 @@ function App() {
   return (
     <div>
       <MainHeader />
-      <Route path="/welcome">
-        <Welcome />
-      </Route>
-      <Route path="/products">
-        <Products />
-      </Route>
-      <Route path="/product-detail/:id">
-        <ProductDetail />
-      </Route>
+      <Switch>
+        <Route path="/welcome">
+          <Welcome />
+        </Route>
+        <Route path="/products" exact>
+          <Products />
+        </Route>
+        <Route path="/products/:id">
+          <ProductDetail />
+        </Route>
+      </Switch>
     </div>
   );
 }
