94420c8671844f0b6022e6ecee3bb9fd2003e21e
simimi129 <simimi129@gmail.com>
2023-01-04 10:23:18 +0100

    reinit

--- src/App.js
+++ src/App.js
@@ -1,27 +1,7 @@
-import { Route, Switch, Redirect } from "react-router-dom";
-import Welcome from "./pages/Welcome";
-import Products from "./pages/Products";
-import MainHeader from "./components/MainHeader";
-import ProductDetail from "./pages/ProductDetail";
-
 function App() {
   return (
     <div>
-      <MainHeader />
-      <Switch>
-        <Route path="/" exact>
-          <Redirect to="/welcome" />
-        </Route>
-        <Route path="/welcome">
-          <Welcome />
-        </Route>
-        <Route path="/products" exact>
-          <Products />
-        </Route>
-        <Route path="/products/:id">
-          <ProductDetail />
-        </Route>
-      </Switch>
+
     </div>
   );
 }
