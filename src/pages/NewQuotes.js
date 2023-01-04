5fc9c6622d17d6e7927671ffbc400e897d28c654
simimi129 <simimi129@gmail.com>
2023-01-04 10:32:30 +0100

    adding routes

--- src/pages/NewQuotes.js
+++
@@ -1,7 +0,0 @@
-import React from "react";
-
-function NewQuotes() {
-  return <div>NewQuotes</div>;
-}
-
-export default NewQuotes;
--- src/pages/QuoteDetail.js
+++ src/pages/QuoteDetail.js
@@ -1,7 +1,15 @@
 import React from "react";
+import { useParams } from "react-router-dom";
 
 function QuoteDetail() {
-  return <div>QuoteDetail</div>;
+  const params = useParams();
+
+  return (
+    <>
+      <h1>Quote Detail</h1>
+      <p>{params.quoteId}</p>
+    </>
+  );
 }
 
 export default QuoteDetail;
