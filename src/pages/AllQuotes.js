360ca7d31451fa5c03d55c8e07d64e21d47d7f8f
simimi129 <simimi129@gmail.com>
2023-01-04 10:47:47 +0100

    adding routes

--- src/pages/AllQuotes.js
+++ src/pages/AllQuotes.js
@@ -1,7 +1,13 @@
 import React from "react";
+import QuoteList from "../components/quotes/QuoteList";
+
+const DUMMY_QUOTES = [
+  { id: "q1", author: "Max", text: "Learning React is fun!" },
+  { id: "q2", author: "Maxi", text: "Learning React is great!" },
+];
 
 function AllQuotes() {
-  return <div>AllQuotes</div>;
+  return <QuoteList quotes={DUMMY_QUOTES} />;
 }
 
 export default AllQuotes;
